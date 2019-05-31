85979a1a588ae4006558195a9d16619670b9f74d
Seb's Go <39569227+seb74813@users.noreply.github.com>
2019-05-30 20:29:15 -0400

    Animation Pt 1

--- Assets/Scripts/SpecialMoves/Dash.cs
+++ Assets/Scripts/SpecialMoves/Dash.cs
@@ -6,11 +6,15 @@ public class Dash : SpecialMoveBase
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
+    private Animator animator;
     private CharacterController2D controller2D;
-
+    private float timerSpecial;
     public override void Special()
     {
         controller2D = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
+        timerSpecial = Time.time + 0.3f;
+        animator.SetBool("Dash", true);
 
         if (controller2D.m_FacingRight)
         {
@@ -19,6 +23,13 @@ public class Dash : SpecialMoveBase
         if (controller2D.m_FacingRight == false)
         {
             rb.AddForce(new Vector2(-1 * speed, 0f));
+        }
+    }
+    private void Update()
+    {
+        if (Time.time > timerSpecial)
+        {
+            animator.SetBool("Dash", false);
         }
     }
 }
